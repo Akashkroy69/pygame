884b59bac8cc2fd3912feafa598b4454ab45fab9
akashkroy69 <akashkroy@hotmail.com>
2023-10-26 06:20:44 +0530

    control added, ERROR: while moving the object it is leaving the trail

--- pygame Lesson1/intro.py
+++ pygame Lesson1/intro.py
@@ -14,18 +14,33 @@ SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 # player step 4
-player = pygame.Rect((0,0,50,50))
+player = pygame.Rect((350,350,50,50))
 
 run = True
 # 2. game loop
 while run:
 #  drawing the player object
- pygame.draw.rect(screen,(117,66,247),player)
+  pygame.draw.rect(screen,(117,66,247),player)
+
+#  control
+  key = pygame.key.get_pressed()
+  if key[pygame.K_UP]:
+    player.y -= 1
+  elif key[pygame.K_DOWN]:
+    player.y += 1
+  elif key[pygame.K_LEFT]:
+    player.x -= 1
+  elif key[pygame.K_RIGHT]:
+    player.x += 1
+
 
     # step 3: event handler
- for event in pygame.event.get():
+  for event in pygame.event.get():
     # logic for closing the window
     if event.type == pygame.QUIT:
         run = False
+#we need to update the screen in each loop so player object that we are creating here becomes visble
+# else as in each loop canvas  
+  pygame.display.update()
 
 pygame.quit()
