d4ae403d8ca54f9eaf3b39283773a20daee41275
akashkroy69 <akashkroy@hotmail.com>
2023-11-03 02:05:42 +0530

    bird added, bird is flying too

--- Flappy_With_Pygame/Falppy In PyGame.py
+++ Flappy_With_Pygame/Falppy In PyGame.py
@@ -10,8 +10,8 @@ from pygame.locals import *
 # initialization
 pygame.init()
 # screen setup
-SCREEN_HEIGHT = 200
-SCREEN_WIDTH = 280
+SCREEN_HEIGHT = 810
+SCREEN_WIDTH = 830
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy In PyGame")
 
@@ -20,9 +20,52 @@ pygame.display.set_caption("Flappy In PyGame")
 background = pygame.image.load(r'Codingal\PYGAME\Flappy_With_Pygame\flapbg.png')
 ground = pygame.image.load(r'Codingal\PYGAME\Flappy_With_Pygame\ground.png')
 
+# 1 adding a bird using sprite
+class Bird(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        # Sprite class has already functions that will updat, draw,load,render the images.
+        # we don't need to call load or blit function externally
+        pygame.sprite.Sprite.__init__(self)
+
+        self.images= []
+        self.index = 0
+        self.counter = 0
+
+
+        for birdNum in range(1,4): 
+            img = pygame.image.load(fr"Codingal\PYGAME\Flappy_With_Pygame\bird{birdNum}.png")
+            self.images.append(img)
+        # this will create a reactable around the image, it's mandatory as the bird image 
+        # might be of unusual shape
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+
+    # To hadnle the animation overriding
+    def update(self):
+        # handles the animation
+        self.counter += 1
+
+        if self.counter>=10:
+          self.counter = 0
+          self.index += 1
+          if self.index>=len(self.images):
+              self.index = 0
+        self.image = self.images[self.index]
+        
+
+
+# 2 using sprite to make a goup of images, here group of bird images.
+#  this creates a list in which we can add our images
+bird_group = pygame.sprite.Group()
+flappyBird = Bird(100, int(SCREEN_HEIGHT/2))
+bird_group.add(flappyBird)
+
+
 # setting the frame rate
 clock = pygame.time.Clock()
 fps = 60
+
 # game variables 
 ground_scroll = 0
 scroll_speed = 3
@@ -40,8 +83,12 @@ while runningStatus:
     # to render the image for background
     screen.blit(background,(0,0))
     # to render image for ground
-    screen.blit(ground,(ground_scroll,158))
+    screen.blit(ground,(ground_scroll,700))
     ground_scroll -= scroll_speed
+
+    # 3 drawing the object from bird group
+    bird_group.draw(screen)
+    bird_group.update()
 
     # code for continuous scroll effect
     if abs(ground_scroll) > 40:
